f04641f65f95e9607549815afddbaff811e02b32
Joaquin <jfarfantorres@gmail.com>
2026-06-16 22:22:33 -0400

    feat: use explicit CSS media query style block to reliably position footer background image Y to 0% on mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -81,8 +81,15 @@ export default function Footer() {
       data-site-footer
       className="relative overflow-x-hidden bg-[#0a192f] text-white lg:min-h-150"
     >
+      <style dangerouslySetInnerHTML={{ __html: `
+        @media (max-width: 440px) {
+          .footer-bg-custom {
+            background-position: 95% 0% !important;
+          }
+        }
+      `}} />
       <div
-        className="pointer-events-none absolute inset-0 bg-no-repeat max-lg:bg-[length:auto_200%] max-lg:bg-[position:78%_66%] max-[440px]:bg-[position:95%_15%] lg:bg-cover lg:bg-[position:right_bottom]"
+        className="pointer-events-none absolute inset-0 bg-no-repeat max-lg:bg-[length:auto_200%] max-lg:bg-[position:78%_66%] lg:bg-cover lg:bg-[position:right_bottom] footer-bg-custom"
         style={{ backgroundImage: `url(${PUBLIC_ASSETS.footer.background})` }}
         aria-hidden
       />
